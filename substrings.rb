d144e39adbf9614310ad3cac26b2b5e8cf63bd75
samanthaahannah <samanthaahannah@outlook.com>
2025-06-25 20:23:30 +0100

    Refactor code with rubocop

--- substrings.rb
+++ substrings.rb
@@ -1,17 +1,17 @@
-dictionary = ["below", "low", "go", "going", "i", "it", "how", "howdy", "you", "are", "do", "doing", "down", "fro", "from"]
+dictionary = %w[below low go going i it how howdy you are do doing down fro
+                from]
 
 def substrings(string, dictionary)
-  mod_str = string.downcase.gsub(/\d+/,"").gsub(/[^\w\s]/,"")
+  mod_str = string.downcase.gsub(/\d+/, "").gsub(/[^\w\s]/, "")
   words = mod_str.split
   selected_words = []
 
   dictionary.each do |substring|
-    words.select{|word| 
-    if word.include?(substring) then 
-      selected_words.push(substring) 
-    end}
+    words.select do |word|
+      selected_words.push(substring) if word.include?(substring)
+    end
   end
 
-   print selected_words.group_by{|substring| substring}.map{|substring,occurance| [substring,occurance.count]}.to_h
+  print(selected_words.group_by { |substring| substring }.transform_values(&:count))
 end
 substrings("Howdy from down below how do you do?", dictionary)
