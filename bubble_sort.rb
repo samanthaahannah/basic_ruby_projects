9f0e2e778b738498fd5cbd71be116176732bfe40
samanthaahannah <samanthaahannah@outlook.com>
2025-06-25 20:26:23 +0100

    Tidy code with rudocop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,15 @@
 def bubble_sort(array)
   array.each_with_index do |num1, num1_idx|
-    array.each_with_index do |num2,   num2_idx|
-        if num1 > num2
-          array[num1_idx],
-          array[num2_idx] =
-          array[num2_idx],
-          array[num1_idx]
-        end
-      
+    array.each_with_index do |num2, num2_idx|
+      next unless num1 > num2
+
+      array[num1_idx],
+      array[num2_idx] =
+        array[num2_idx],
+        array[num1_idx]
     end
   end
-    print array.reverse()
+  print array.reverse
 end
 
-bubble_sort([3,10,78,9,62,8])
+bubble_sort([3, 10, 78, 9, 62, 8])
